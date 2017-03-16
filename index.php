50799ba59b2c750b6248de8822a3198ccbf2455d
Lemon <wf3344179@vip.qq.com>
2017-03-16 17:04:33 +0800

    titile change

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $result = mysqli_fetch_all(mysqli_query($a, $sql));
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <title>Bootstrap 101 Template</title>
+    <title>My bilibili</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
